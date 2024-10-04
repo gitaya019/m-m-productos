dbfdbde4232f5067ff02d9156343906814a0fd81
gitaya019 <ayalajhoan24@gmail.com>
2024-10-04 14:25:55 -0500

    Ajustes de https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if(config('app.env') === 'local'){
-            $this->app['request']->server->set('HTTPS', true);
-        }
+       // if(config('app.env') === 'local'){
+       //     $this->app['request']->server->set('HTTPS', true);
+       // }
     }
 
     /**
@@ -21,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
